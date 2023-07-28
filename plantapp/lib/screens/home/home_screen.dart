a5e27d71fffbadd325acf11ee00e694d1c5b790f
ngtram1 <vo9418172@gmail.com>
2023-07-28 09:19:56 +0700

    Vo Ngoc Tram

---
+++ plantapp/lib/screens/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:plantapp/components/my_bottom_nav_bar.dart';
+import 'package:plantapp/screens/home/components/body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: const Body(),
+      bottomNavigationBar: const MyBottomNavBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
